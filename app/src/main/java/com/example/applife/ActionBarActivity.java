a1119aa442edcb5f445f9ccdef5412f8b3556eaa
Cristiane Castilho <criscastilho.macedo@gmail.com>
2019-11-13 16:14:35 -0300

    Commit inicial do projeto

---
+++ app/src/main/java/com/example/applife/ActionBarActivity.java
@@ -0,0 +1,7 @@
+package com.example.applife;
+
+import android.os.Bundle;
+
+abstract class ActionBarActivity {
+    protected abstract void onCreate(Bundle savedInstanceState);
+}
--- app/src/main/java/com/example/applife/MainActivity.java
+++ app/src/main/java/com/example/applife/MainActivity.java
@@ -5,7 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
     SharedPreferences sharedPreferences;
@@ -21,8 +22,44 @@ public class MainActivity extends AppCompatActivity {
         String apelido = sharedPreferences.getString("apelido","");
         String email = sharedPreferences.getString("email","");
         String sexo = sharedPreferences.getString("sexo","");
-        TextView textView = this.findViewById(R.id.message_pessoa);
 
-        textView.setText("Bem vindo, "+apelido+sexo+email);
+
+
+//        textView.setText("Bem vindo, "+apelido+email+sexo);
+        final ImageView imagemPessoa = findViewById(R.id.imagempessoa);
+        final ImageView imagemEstudo = findViewById(R.id.Icone_Estudo);
+        final ImageView imagemAtividade = findViewById(R.id.Icone_Atividade_Fisica);
+        final ImageView imagemOcio = findViewById(R.id.Icone_Ocio);
+        final ImageView imagemTrabalho = findViewById(R.id.Icone_Trabalho);
+
+        imagemEstudo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                imagemPessoa.setImageResource(R.drawable.img_estudof);
+            }
+        });
+        imagemAtividade.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                imagemPessoa.setImageResource(R.drawable.img_atividadef);
+            }
+        });
+        imagemTrabalho.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                imagemPessoa.setImageResource(R.drawable.img_trabalhof);
+            }
+        });
+        imagemOcio.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (true){//testar se do sexo masculino
+                    imagemPessoa.setImageResource(R.drawable.imagem_ociom);
+                } else {
+                    imagemPessoa.setImageResource(R.drawable.imagem_ociom);
+                }
+
+            }
+        });
     }
 }
